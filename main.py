333efe74450f5e0f8f1c3c962165bb4e66924863
𝐉ᴀᴅᴇɴ 𝐀ғʀx <tawandamahachi07@gmail.com>
2026-05-11 22:26:04 +0200

    @mr-afrix

--- main.py
+++ main.py
@@ -2348,6 +2348,10 @@ async def post_init(application):
 
 
 if __name__ == "__main__":
+    import asyncio
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
     db.init()
 
     application = (
